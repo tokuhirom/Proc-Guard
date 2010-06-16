1eb267e69b189607c58ac1267802e4ceda966940
tokuhirom <tokuhirom@gmail.com>
2010-06-16 20:41:11 +0900

    initial import

---
+++ lib/Proc/Guard.pm
@@ -0,0 +1,39 @@
+package Proc::Guard;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Proc::Guard -
+
+=head1 SYNOPSIS
+
+  use Proc::Guard;
+
+=head1 DESCRIPTION
+
+Proc::Guard is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
